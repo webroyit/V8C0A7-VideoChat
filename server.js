d69467658a73cdc56cc28d1ba49f1f4c5a24e6bf
royweb <xytank@gmail.com>
2020-08-07 14:31:03 -0400

    Add a route for room and use uuid to create random roomId

--- server.js
+++ server.js
@@ -5,4 +5,18 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+// For random roomId
+const { v4: uuidV4 } = require('uuid');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidV4()}`);
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room });
+});
+
 server.listen(3000);
