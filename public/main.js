acab9ab8f4c1149418d485b4a61a19a5ddea7a4b
royweb <xytank@gmail.com>
2020-08-12 12:59:02 -0400

    Display a message when someone joins the room

--- public/main.js
+++ public/main.js
@@ -2,3 +2,7 @@ const socket = io('/');
 
 // Send an event to the server
 socket.emit('join-room', ROOM_ID, 10);
+
+socket.on('user-connected', userId => {
+    console.log('User connected: ' + userId);
+})
