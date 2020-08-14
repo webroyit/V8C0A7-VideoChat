6f8e9843c8992ff0bc25177471e419de789c601c
royweb <xytank@gmail.com>
2020-08-14 13:06:51 -0400

    Enable access to your video

--- public/main.js
+++ public/main.js
@@ -1,7 +1,18 @@
 const socket = io('/');
+const videoGrid = document.getElementById('video-grid');
 const myPeer = new Peer(undefined, {
     host: '/',
     port: '3001'
+});
+const myVideo = document.createElement('video');
+myVideo.muted = true;       // Disable on listening to your own video
+
+// Conntect to your video
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    console.log(stream);
 })
 
 myPeer.on('open', id => {
@@ -10,4 +21,4 @@ myPeer.on('open', id => {
 
 socket.on('user-connected', userId => {
     console.log('User connected: ' + userId);
-})
+});
