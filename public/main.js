34da29f672409965674a887b5837adf60452116b
royweb <xytank@gmail.com>
2020-08-23 13:03:25 -0400

    Install Peer

--- public/main.js
+++ public/main.js
@@ -1,8 +1,9 @@
 const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
 const myPeer = new Peer(undefined, {
+    path: '/peerjs',
     host: '/',
-    port: '3001'
+    port: '443'
 });
 const myVideo = document.createElement('video');
 myVideo.muted = true;       // Disable on listening to your own video
--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
+
 // For random roomId
 const { v4: uuidV4 } = require('uuid');
+
+app.use('/peerjs', peerServer);
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -33,4 +40,4 @@ io.on('connection', socket => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log(`Server running in ${PORT}`));
+server.listen(PORT, console.log(`Server running in ${PORT}`));
